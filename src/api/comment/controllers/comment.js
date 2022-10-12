2421ce8db5ad455b70e5c58c3c72dca37a617e5f
pasakornCRMC <pasakorn@crmc.consulting>
2022-10-13 02:38:50 +0700

    updated api and user permission

--- src/api/comment/controllers/comment.js
+++ src/api/comment/controllers/comment.js
@@ -6,4 +6,20 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::comment.comment');
+module.exports = createCoreController('api::comment.comment', ({strapi}) => ({
+    async create(ctx) {
+        const user = ctx.state.user;
+        // 2
+        ctx.request.body.data = {
+            ...ctx.request.body.data,
+            author: user.username,
+            user: user.id
+        };
+  
+        // 3
+        const response = await super.create(ctx);
+    
+        // 4
+        return response;
+        }
+}));
